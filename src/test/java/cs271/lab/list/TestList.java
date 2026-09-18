3bfb3d78686a3aacd44532f910098857fec57e8f
sugwu <sugwu@luc.edu>
2026-09-17 19:37:39 -0500

    Finalize Project 2 tests

--- src/test/java/cs271/lab/list/TestList.java
+++ src/test/java/cs271/lab/list/TestList.java
@@ -19,7 +19,7 @@ public class TestList {
 
   @BeforeEach
   public void setUp() throws Exception {
-    list = new LinkedList<Integer>();
+    list = new ArrayList<Integer>();
     // TODO Question: Also try with a LinkedList - does it make any difference?
   }
 
